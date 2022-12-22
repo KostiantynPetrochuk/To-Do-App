7766187aa4c58d86b5474da9c1babf2d55305cd3
kostiantyn <firstpickpudge@gmail.com>
2022-12-22 09:55:38 +0200

    update

--- src/features/Tasks/TasksList.jsx
+++ src/features/Tasks/TasksList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import List from "@mui/material/List";
@@ -14,72 +14,79 @@ import { selectActiveFilter } from "../Filters/filtersSlice";
 import { selectVisibleTasks, deleteTask, toggleStatus } from "./tasksSlice";
 import { Alert } from "../../components/Notice";
 
+//---helper---
+const getId = (target) => {
+  const element = target;
+  const elementId = element.dataset.id;
+
+  if (elementId) {
+    return elementId;
+  }
+  return getId(element.parentNode);
+};
+//---
+const getCommand = (target) => {
+  const element = target;
+  const elementCommand = element.dataset.command;
+
+  if (elementCommand) {
+    return elementCommand;
+  }
+  return getCommand(element.parentNode);
+};
+//---
+
 export default function TasksList() {
-  const [checked, setChecked] = useState([0]);
-
-  const handleToggle = (value) => () => {
-    const currentIndex = checked.indexOf(value);
-    const newChecked = [...checked];
-
-    if (currentIndex === -1) {
-      newChecked.push(value);
-    } else {
-      newChecked.splice(currentIndex, 1);
-    }
-
-    setChecked(newChecked);
-  };
-
   const dispatch = useDispatch();
   const activeFilter = useSelector(selectActiveFilter);
   const tasksList = useSelector((state) =>
     selectVisibleTasks(state, activeFilter)
   );
+
+  const handleDeleteTask = (id) => {
+    dispatch(deleteTask(id));
+  };
+
+  const handleToggleTaskStatus = (id) => {
+    dispatch(toggleStatus(id));
+  };
+
+  const handleListClick = (event) => {
+    const targetTaskId = getId(event.target);
+    const targetCommand = getCommand(event.target);
+    switch (targetCommand) {
+      case "toggle":
+        console.log("toggle");
+        handleToggleTaskStatus(targetTaskId);
+        break;
+      case "delete":
+        console.log("delete");
+        handleDeleteTask(targetTaskId);
+        break;
+    }
+  };
+
+  const listItems = tasksList.map((task) => {
+    const labelId = `checkbox-list-label-${task.id}`;
+    const textDecoration = task.isDone ? "line-through" : "none";
+
+    return (
+      <Task
+        key={task.id}
+        task={task}
+        labelId={labelId}
+        textDecoration={textDecoration}
+      />
+    );
+  });
+
   if (tasksList.length > 0) {
     return (
-      <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-        {tasksList.map((task) => {
-          const labelId = `checkbox-list-label-${task.id}`;
-          const textDecoration = task.isDone ? "line-through" : "none";
-          console.log(task);
-          return (
-            <ListItem
-              key={task.id}
-              secondaryAction={
-                <IconButton
-                  edge="end"
-                  aria-label="comments"
-                  onClick={() => dispatch(deleteTask(task.id))}
-                >
-                  <DeleteIcon />
-                </IconButton>
-              }
-              disablePadding
-            >
-              <ListItemButton
-                role={undefined}
-                onClick={handleToggle(task.id)}
-                dense
-              >
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={task.isDone}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ "aria-labelledby": labelId }}
-                    onChange={() => dispatch(toggleStatus(task.id))}
-                  />
-                </ListItemIcon>
-                <ListItemText
-                  id={labelId}
-                  primary={`${task.body}`}
-                  sx={{ textDecoration: textDecoration }}
-                />
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
+      <List
+        onClick={handleListClick}
+        sx={{ width: "100%", bgcolor: "background.paper" }}
+      >
+        {listItems}
       </List>
     );
   } else {
@@ -90,3 +97,38 @@ export default function TasksList() {
     );
   }
 }
+
+//----
+const Task = (props) => {
+  const { task, labelId, textDecoration } = props;
+
+  return (
+    <ListItem
+      data-id={task.id}
+      key={task.id}
+      secondaryAction={
+        <IconButton edge="end" aria-label="comments" data-command="delete">
+          <DeleteIcon />
+        </IconButton>
+      }
+      disablePadding
+    >
+      <ListItemButton data-command="toggle" role={undefined} dense>
+        <ListItemIcon>
+          <Checkbox
+            edge="start"
+            checked={task.isDone}
+            tabIndex={-1}
+            disableRipple
+            inputProps={{ "aria-labelledby": labelId }}
+          />
+        </ListItemIcon>
+        <ListItemText
+          id={labelId}
+          primary={`${task.body}`}
+          sx={{ textDecoration: textDecoration }}
+        />
+      </ListItemButton>
+    </ListItem>
+  );
+};
